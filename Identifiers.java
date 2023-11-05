a1df474c01e019fa5aabe2f465e58546ca1c61ab
kajal <kajal@gmail.com>
2023-11-05 13:45:27 +0530

    ..

---
+++ Identifiers.java
@@ -0,0 +1,24 @@
+class Identifiers{
+    public static void main(String[] args) {
+        
+
+        byte roll= 123;
+        String s = " arun's rollno is "+roll;
+        System.out.println(s);
+
+        byte num = Byte.MAX_VALUE;
+        System.out.println("maximum value "+num);
+        
+        num = Byte.MIN_VALUE;
+        System.out.println("minnimum value "+num);
+
+        //data-type identifier  = 100
+        // int number = 100;
+        // number=110;
+        // String s="jhsafbsfb";
+        // System.out.println(s);
+
+
+
+    }
+}
---
+++ Test.java
@@ -0,0 +1,21 @@
+class Test{
+
+
+    public static void main(String []args) {
+        
+
+        
+        //keywords
+
+       // System.out.println("Hello world"); single line comment
+
+       /*
+       this is 
+       a  multine 
+       comment
+        */
+
+    }
+
+
+}
