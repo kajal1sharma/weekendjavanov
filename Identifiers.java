c5af9cd7c44f2b711d56d4d04b43da722018b845
kajal <kajal@gmail.com>
2023-11-05 14:35:55 +0530

    ..

--- Identifiers.java
+++ Identifiers.java
@@ -1,18 +1,19 @@
 class Identifiers{
     public static void main(String[] args) {
         
-
-        byte roll= 123;
-        String s = " arun's rollno is "+roll;
+        String s ="first \n"+"second";
         System.out.println(s);
+        // byte roll= 123;
+        // String s = " arun's rollno is "+roll;
+        // System.out.println(s);
 
-        byte num = Byte.MAX_VALUE;
-        String s2 = "maximum value "+num;
-        System.out.println(s2);
+        // byte num = Byte.MAX_VALUE;
+        // String s2 = "maximum value "+num;
+        // System.out.println(s2);
         
-        num = Byte.MIN_VALUE;
-        s2="minnimum value "+num;
-        System.out.println(s2);
+        // num = Byte.MIN_VALUE;
+        // s2="minnimum value "+num;
+        // System.out.println(s2);
 
         //data-type identifier  = 100
         // int number = 100;
