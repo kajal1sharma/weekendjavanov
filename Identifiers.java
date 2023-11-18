f530cc5856a0fc6468992c3b62c9db7bff8a9dd1
kajal <kajal@gmail.com>
2023-11-18 13:53:31 +0530

    ..

--- Identifiers.java
+++ Identifiers.java
@@ -1,8 +1,51 @@
 class Identifiers{
     public static void main(String[] args) {
-        
-        String s ="first \n"+"second";
+
+        char c ='a';
+        c=48;
+        System.out.println(c);
+
+        char ch1 ='k';
+        char ch2 ='a';
+        char ch3 ='j';
+        char ch4 ='a';
+        char ch5 ='l';
+
+        String s= ""+ch1+ch2+ch3+ch4+ch5;
+        // String s2= "kajal";
         System.out.println(s);
+        
+        // byte  num1 = 10;
+        // short num2 = 20;
+        // int result =num1+num2;
+        // result =num2+num2;
+        // System.out.println(result);
+        // int a = 10 ;
+        // int b = 20 ;
+
+        // int c = a +  b;
+        // String result  = a+"";//"10"
+        // result  = result + b;
+        // System.out.println(c);
+        // System.out.println(result);
+        //1020
+        //30
+
+        // short num = Short.MIN_VALUE;//1byte = 7 + 1 bits 
+        // String resultstr = num +" => minimum value in short";
+        // byte num2 = Byte.MIN_VALUE;//2bytes = 15+1 bits
+        // String resultstr2 = num2 + " => minimum value of type byte";
+        // int num3 = Integer.MIN_VALUE;//4bytes = 31+1 bits
+        // String resultstr3 = num3 +" => minimm value in int";
+        // long num4  =  Long.MIN_VALUE;//8bytes =  63+1 bits
+        // String resultstr4 =num4 +" => minimm value in int";
+        // System.out.println(resultstr);
+        // System.out.println(resultstr2);
+        // System.out.println(resultstr3);
+        // System.out.println(resultstr4);
+
+        // String s ="first \n"+"second";
+        // System.out.println(s);
         // byte roll= 123;
         // String s = " arun's rollno is "+roll;
         // System.out.println(s);
---
+++ Identifiers2.java
@@ -0,0 +1,46 @@
+public class Identifiers2 {
+    public static void main(String[] args) {
+        
+        int a;//dec
+        
+        System.out.println(a);
+        int b=a+10;
+        a=90;//ini
+
+        char ch='1';//declaration and initialization
+        ch ='2';
+        ch='a';
+
+        // primitives are storage space
+
+        // int a= 97;
+        // a=90;
+
+        // int a=90;
+        // char c='c';
+    
+        // String s="djkfnkasndf0";
+        // s=s.toUpperCase();
+        // System.out.println(s);
+
+        
+        
+
+        //int b;
+        //b=90;
+        
+
+        // Identifier
+        // int a;//declaration
+        // a=90;//definition, initialization  (when we give some value to a variable for the first time )
+        //a is the name of a  variable
+        //identifier is the name of any class, function Interface,refernce , varibles
+        // primitives => int byte short long double float char boolean 
+        // varibles => are the names of primitives 
+        // because their values can change
+
+       // a=190;//definition
+
+        // x=10+y;
+    }   
+}
