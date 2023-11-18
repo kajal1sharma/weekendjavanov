ffd614a561acef794df28b7556c5dd59adbe587b
kajal <kajal@gmail.com>
2023-11-18 14:11:59 +0530

    ..

--- Identifiers2.java
+++ Identifiers2.java
@@ -1,15 +1,44 @@
 public class Identifiers2 {
     public static void main(String[] args) {
         
-        int a;//dec
-        
-        System.out.println(a);
-        int b=a+10;
-        a=90;//ini
 
-        char ch='1';//declaration and initialization
-        ch ='2';
-        ch='a';
+    // lowercase uppercase alphabets
+       
+        int firstNumber;
+        // digits can be used but they can not be first char
+
+        int 1number;//wrong
+        int number1;//corrrect
+        // underscore
+
+        int _first_number;
+        int first_number;
+        // $ dollar symbol can be used
+        int first$number;
+        int $firstNumber;
+
+        int firsy*number;//other symbols cannot be used
+        int first number;//wrong
+
+        int abc2__345__$jdfgjdWRWERewc;
+
+        // Java Convention
+
+        // variables, function , refernces => lastName
+        //class ,interface  => AccountDepartment
+        //Enum = > CITY
+
+        
+        // int a;//dec
+        
+        // System.out.println(a);
+        // a=90;//ini
+        // int b=a+10;
+       
+
+        // char ch='1';//declaration and initialization
+        // ch ='2';
+        // ch='a';
 
         // primitives are storage space
 
