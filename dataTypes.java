ad9429eeaa71c2ac763967a01326d7146f2e54e4
kajal <kajal@gmail.com>
2023-11-25 14:29:17 +0530

    ..

--- dataTypes.java
+++ dataTypes.java
@@ -2,18 +2,37 @@ public class dataTypes {
     public static void main(String[] args) {
         
 
+        int a = 0X11;
+        System.out.println(a);
+        int b = 045;
+        System.out.println(b);
+        int c= 0b0101010101;
+        System.out.println(c);
+
+        
+        // boolean b= false;
+        // b=true;
+        // // boolean literals => true and false
+        // int a=90;
+        // a=12;
+        // integral literal
+         
+        // float =>precision less double
+
         // integral data  types => byte(1), short(2) int(4) long(8)
         // floating point data types => float(4)  double(8)
 
-        double first_num = 27.45;
+        // double first_num = 27.45;
 
 
-        float max= Float.MAX_VALUE;
-        System.out.println(max+"  float max");
-         float second_num = 34.56f;
-         double d= second_num;
-        long l= Long.MAX_VALUE;
-        System.out.println(l+" long max");
+        // float max= Float.MAX_VALUE;
+        // System.out.println(max+"  float max");
+        //  float second_num = 34.56f;
+        //  double d= second_num;
+        // long l= Long.MAX_VALUE;
+        // System.out.println(l+" long max");
+
+
 
     }
 }
