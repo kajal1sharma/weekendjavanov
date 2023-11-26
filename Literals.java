fe16e7e9dafc9082aa818e6fc56d37bb6708d560
kajal <kajal@gmail.com>
2023-11-26 13:32:25 +0530

    ..

---
+++ Literals.java
@@ -0,0 +1,10 @@
+class Literals{
+    public static void main(String[] args) {
+        // integral literal
+
+        int hexNum = 0x60;
+        System.out.println(hexNum);
+        int octNum = 0104;
+        System.out.println(octNum);
+    }
+}
