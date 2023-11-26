c11491b9f2440520a4fe1cf3d1c6b55d5cdc25af
kajal <kajal@gmail.com>
2023-11-26 14:01:57 +0530

    ..

--- Literals.java
+++ Literals.java
@@ -2,9 +2,29 @@ class Literals{
     public static void main(String[] args) {
         // integral literal
 
-        int hexNum = 0x60;
-        System.out.println(hexNum);
-        int octNum = 0104;
-        System.out.println(octNum);
+
+        // long num = 1_3_1434;;
+        // every integral literal by default is of type int
+        //every floating point literal is of type double
+        
+        long num = 21474836477l;
+        byte s= 12;
+        System.out.println(Integer.MAX_VALUE);
+        // System.out.println(num);
+        // int a=0010101;//octal
+        // int d=100;//dec
+        // int b=10101010;//dec
+        // int c=1467;
+        // System.out.println(a);
+        // System.out.println(d);
+        // System.out.println(b);
+        // System.out.println(c);
+        // int hexNum = 0x60;
+        // System.out.println(hexNum);
+        // int octNum = 0104;
+        // System.out.println(octNum);
+        // int binnum = 0B01010101;
+        // int decinum =8786473;
+
     }
 }
